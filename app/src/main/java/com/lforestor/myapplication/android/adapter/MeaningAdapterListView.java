ed282671902d4ec18f9ce136366edc8d87a62931
nchh12 <hoanghuync12@gmail.com>
2021-07-11 15:09:51 +0700

    make synonyms clickable

--- app/src/main/java/com/lforestor/myapplication/android/adapter/MeaningAdapterListView.java
+++ app/src/main/java/com/lforestor/myapplication/android/adapter/MeaningAdapterListView.java
@@ -1,14 +1,22 @@
 package com.lforestor.myapplication.android.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.BaseAdapter;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.core.content.res.ResourcesCompat;
 
@@ -74,71 +82,82 @@ public class MeaningAdapterListView extends BaseAdapter {
             convertView = layoutInflater.inflate(R.layout.activity_listview_dictionary, null);
             viewHolder = new MeaningAdapterListView.ViewHolder();
             //define
-            viewHolder.wordNumber = convertView.findViewById(R.id.meaningNumber);
-            viewHolder.wordType = convertView.findViewById(R.id.meaningWordType);
-            viewHolder.wordDefinition = convertView.findViewById(R.id.meaningDefinition);
-            viewHolder.wordExample = convertView.findViewById(R.id.meaningExample);
-            viewHolder.wordSynonym = convertView.findViewById(R.id.meaningSynonym);
-
-            viewHolder.labelExample = convertView.findViewById(R.id.meaningExampleLabel);
-            viewHolder.labelSynonym = convertView.findViewById(R.id.meaningSynonymLabel);
-            viewHolder.lineExample = convertView.findViewById(R.id.meaningExampleLine);
-            viewHolder.lineSynonym = convertView.findViewById(R.id.meaningSynonymLine);
-
-            Typeface typeface = ResourcesCompat.getFont(context, R.font.math_tapping);
-            viewHolder.wordNumber.setTypeface(typeface);
-            viewHolder.wordType.setTypeface(typeface);
-            viewHolder.labelExample.setTypeface(typeface);
-            viewHolder.labelSynonym.setTypeface(typeface);
+            mapping(viewHolder, convertView);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (MeaningAdapterListView.ViewHolder) convertView.getTag();
         }
+
+        //get string
         String type = currentWord.getFieldResults(FieldEnums.partOfSpeech, position);
         String definition = currentWord.getFieldResults(FieldEnums.definition, position);
         ArrayList<String> examples = currentWord.getFieldResultsArray(FieldEnums.examples, position);
         ArrayList<String> synonyms = currentWord.getFieldResultsArray(FieldEnums.synonyms, position);
         Double rate = Double.parseDouble(currentWord.getFieldSafely(FieldEnums.frequency));
         int frequencyPoint = (int) (rate / WordsRepo.MAX_FREQUENCY_POINT * 100);
-//        Log.d("@@@", results);
-//        Log.d("@@@", test.get(0));
-//        Log.d("@@@", test2);
 
+        //set content
         viewHolder.wordNumber.setText(position + 1 + ".");
         viewHolder.wordType.setText(type);
         viewHolder.wordDefinition.setText(definition);
 
         String exp = "";
-        String syn = "";
         for (int i = 0; !examples.isEmpty() && i < examples.size(); i++){
             if (i < examples.size() - 1)
                 exp += "\"" + examples.get(i) + "\"" + "\n";
             else
                 exp += "\"" + examples.get(i) + "\"";
         }
-        for (int i = 0; !synonyms.isEmpty() && i < synonyms.size(); i++){
-            if (i < synonyms.size() - 1)
-                syn += synonyms.get(i) + ", ";
-            else
-                syn += synonyms.get(i);
-            }
-
         if (exp != "")
             viewHolder.wordExample.setText(exp);
         else {
             viewHolder.lineExample.setVisibility(View.GONE);
         }
-        if (syn != "")
-            viewHolder.wordSynonym.setText(syn);
-        else{
-            viewHolder.lineSynonym.setVisibility(View.GONE);
+
+        if (!synonyms.isEmpty()) {
+            SpannableStringBuilder syn = new SpannableStringBuilder("");
+            for (int i = 0; !synonyms.isEmpty() && i < synonyms.size(); i++) {
+                syn.append(synonyms.get(i));
+                int pos = i;
+                syn.setSpan(new ClickableSpan() {
+                    @Override
+                    public void onClick(View widget) {
+                        Toast.makeText(context, synonyms.get(pos),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }, syn.length() - synonyms.get(i).length(), syn.length(), 0);
+                if (i < synonyms.size() - 1)
+                    syn.append(", ");
+            }
+            viewHolder.wordSynonym.setMovementMethod(LinkMovementMethod.getInstance());
+            viewHolder.wordSynonym.setText(syn, TextView.BufferType.SPANNABLE);
         }
+        else
+            viewHolder.lineSynonym.setVisibility(View.GONE);
+
         //color
         int colorHash = getColor(frequencyPoint);
         viewHolder.labelExample.setTextColor(colorHash);
         viewHolder.labelSynonym.setTextColor(colorHash);
         return convertView;
+    }
 
+    void mapping(ViewHolder viewHolder, View convertView){
+        viewHolder.wordNumber = convertView.findViewById(R.id.meaningNumber);
+        viewHolder.wordType = convertView.findViewById(R.id.meaningWordType);
+        viewHolder.wordDefinition = convertView.findViewById(R.id.meaningDefinition);
+        viewHolder.wordExample = convertView.findViewById(R.id.meaningExample);
+        viewHolder.wordSynonym = convertView.findViewById(R.id.meaningSynonym);
+        viewHolder.labelExample = convertView.findViewById(R.id.meaningExampleLabel);
+        viewHolder.labelSynonym = convertView.findViewById(R.id.meaningSynonymLabel);
+        viewHolder.lineExample = convertView.findViewById(R.id.meaningExampleLine);
+        viewHolder.lineSynonym = convertView.findViewById(R.id.meaningSynonymLine);
+
+        Typeface typeface = ResourcesCompat.getFont(context, R.font.math_tapping);
+        viewHolder.wordNumber.setTypeface(typeface);
+        viewHolder.wordType.setTypeface(typeface);
+        viewHolder.labelExample.setTypeface(typeface);
+        viewHolder.labelSynonym.setTypeface(typeface);
     }
 
     static class ViewHolder {
@@ -146,10 +165,10 @@ public class MeaningAdapterListView extends BaseAdapter {
         TextView wordType;
         TextView wordDefinition;
         TextView wordExample;
-        TextView wordSynonym;
         TextView labelExample;
         TextView labelSynonym;
         RelativeLayout lineExample;
         RelativeLayout lineSynonym;
+        TextView wordSynonym;
     }
 }
