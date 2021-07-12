b412c5380cf2f392d895fcebb6dd8a2f27b4de50
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-07-12 21:33:34 +0700

    build aab

--- app/src/main/java/com/lforestor/myapplication/android/utils/JSONParam.kt
+++ app/src/main/java/com/lforestor/myapplication/android/utils/JSONParam.kt
@@ -6,7 +6,7 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
-class JSONParam(string: String) {
+class JSONParam(string: String?) {
     private var intance: JSONObject? = null
 
     constructor() : this("{}") {}
